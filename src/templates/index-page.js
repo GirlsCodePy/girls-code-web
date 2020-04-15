7cde54db503ee81a95b871a20aab603b880ad183
Gabriela Gaona <gabriela@codium.com.py>
2020-04-14 21:38:17 -0400

    agrego fuente geomanist

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -72,7 +72,7 @@ export const IndexPageTemplate = ({
               <div className="content">
                 <div className="content">
                   <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
+                    <h1>{mainpitch.title}</h1>
                   </div>
                   <div className="tile">
                     <h3 className="subtitle">{mainpitch.description}</h3>
