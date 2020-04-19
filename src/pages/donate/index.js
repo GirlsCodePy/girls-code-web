52c772db3e331ff5448dc055c8d182d3a7cdf86e
Eliana Ferreira <elianaef817@gmail.com>
2020-04-18 22:40:02 -0400

    agregados como componentes el boton de donacion y la vista correspondiente

--- src/pages/donate/index.js
+++ src/pages/donate/index.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react'
-
-import { withTranslation } from 'react-i18next';
  
 import Layout from '../../components/Layout'
-import FlowerLogo from '../../img/flower.svg';
-
+import Donate from '../../components/Donate';
 
 
 const DonatePage  = class extends Component {
-  
   render() {
-
-    const { t } = this.props;
-
     return (
       <Layout>
-        <div
-          className="section margin-top-0 gc-centered-content"
-        >
-          <img
-            src={FlowerLogo}
-            alt="Flower"
-            style={{ width: '5em', height: '5em' }}
-          />
-          <h1 className="gc-subtitle">
-            {t("bePart")}
-          </h1>
-          <p className="gc-pink-text">{t("sendMessage")}</p>
-          <div className="gc-purple-button">
-            {t("donateUs")}
-          </div>
-        </div>
+        <Donate />
       </Layout>
     )
   }
 }
 
-export default withTranslation()(DonatePage)
+export default DonatePage
