36372965c643da028d62137eb9b209cf524a371e
Beatriz Sandoval <mbsandoval10@gmail.com>
2020-05-09 12:55:12 -0400

    Improves button styling

--- src/components/DonateButton.js
+++ src/components/DonateButton.js
@@ -6,7 +6,7 @@ const DonateButton = class extends Component {
     return (
       <React.Fragment>
         <button 
-            className="gc-purple-button"
+            className="gc-button is-dark-purple"
             onClick={()=> window.open("https://www.patreon.com/", "_blank")}>
           {this.props.label}
         </button>
