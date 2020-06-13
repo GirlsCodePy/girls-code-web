8c03e94d930b06182ad7c82ccb05dd21324f314e
Gabriela Gaona <gabriela@codium.com.py>
2020-06-13 19:02:46 -0400

    fix padding in ultra wide screens

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -7,38 +7,38 @@ import { withTranslation } from "react-i18next"
 
 export const WhatWeDo = ({t}) => (
   <div className="column-container">
-    <div class="has-text-centered">
+    <div className="has-text-centered">
       <h1 className="principal-title separation">{t("whatWeDo")}</h1>
     </div>
-    <div class="columns columns-centered">
-      <div class="column">
-        <div class="box flex-box">
+    <div className="columns columns-centered">
+      <div className="column">
+        <div className="box flex-box">
           <img
             src={talleres}
             alt="cursos"
-            class="img-size"
+            className="img-size"
           />
-          <div class="box-text">{t("workshops")}</div>
+          <div className="box-text">{t("workshops")}</div>
         </div>
       </div>
-      <div class="column">
-        <div class="box flex-box">
+      <div className="column">
+        <div className="box flex-box">
           <img
             src={cursos}
             alt="cursos"
-            class="img-size"
+            className="img-size"
           />
-          <div class="box-text">{t("courses")}</div>
+          <div className="box-text">{t("courses")}</div>
         </div>
       </div>
-      <div class="column">
-        <div class="box flex-box">
+      <div className="column">
+        <div className="box flex-box">
           <img
             src={mentorias}
             alt="cursos"
-            class="img-size"
+            className="img-size"
           />
-          <div class="box-text">{t("mentorship")}</div>
+          <div className="box-text">{t("mentorship")}</div>
         </div>
       </div>
     </div>
