46f94df91079df9d0dac4e601045cd0414a4cd23
Beatriz Sandoval <mbsandoval10@gmail.com>
2020-11-26 13:37:05 -0300

    Improved partner component - Added i18n support - Fixed eslint warnings

--- src/components/Partner/Partner.js
+++ src/components/Partner/Partner.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import {withTranslation} from "react-i18next";
 import './styles.sass';
 
-export const Partner = ({}) => (
+export const Partner = ({t}) => (
     <div>
-        <div className="principal-title aliances" >Alianzas</div>
+        <div className="principal-title aliances" >{t("partners")}</div>
         <div className="wrapper">
-            <div><img src= "/img/koga-impact-lab.png" alt="enfoque niñez"></img></div>
-            <div><img src="/img/enfoque-ninez.png"></img></div> 
-            <div><img src="/img/mitic.png"></img></div>
-            <div><img src="/img/campus-party.png"></img></div>
-            <div><img src="/img/paraguay-educa.png"></img></div>
-            <div><img src="/img/django-girls.png"></img></div>
-            <div><img src="/img/global-shapers.png"></img></div>
+            <div><img src= "/img/koga-impact-lab.png" alt="koga impact lab" /></div>
+            <div><img src="/img/enfoque-ninez.png" alt="enfoque niñez" /></div>
+            <div><img src="/img/mitic.png" alt="mitic" /></div>
+            <div><img src="/img/campus-party.png" alt="campus party" /></div>
+            <div><img src="/img/paraguay-educa.png" alt="paraguay educa" /></div>
+            <div><img src="/img/django-girls.png" alt="django girls" /></div>
+            <div><img src="/img/global-shapers.png" alt="global shapers" /></div>
         </div>
     </div>
 
 );
 
-export default Partner
+export default withTranslation()(Partner);
