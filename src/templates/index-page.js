dba84467311b2c1249749b092af349fd26f697bf
Gabriela Gaona <gabriela@codium.com.py>
2020-12-10 19:15:05 -0300

    separate component for navbar

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
+import {graphql } from "gatsby";
 
 import Layout from "../components/Layout";
 import FlowerLogo from "../img/flower.svg";
@@ -50,33 +50,7 @@ export const IndexPageTemplate = ({
                   buttonLabel="writeUs"
                   buttonLink="/contact"
                 />
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                {/* <Features gridItems={intro.blurbs} /> */}
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  {/* <BlogRoll /> */}
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
+                
               </div>
             </div>
           </div>
@@ -137,7 +111,6 @@ export const pageQuery = graphql`
             }
           }
         }
-        heading
         mainpitch {
           title_en
           title_es
@@ -148,20 +121,6 @@ export const pageQuery = graphql`
           label
         }
         description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
