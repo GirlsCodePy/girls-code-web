9c8048c21d2f5f92ce846a19bf6941d45070606c
samuel.ruiz <samuel.ruiz@willdom.com>
2022-08-25 15:12:51 -0400

    Elimina rutas del template no usadas

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -51,11 +51,6 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
-          }
-        }
         mainpitch {
           title_en
           title_es
