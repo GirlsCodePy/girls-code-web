65bbdc9b3ef45a16b1b37dbe2881e9cb3a7e1ed1
Sergio Sotelo <sergiosrtd@gmail.com>
2022-09-04 23:25:23 -0400

    Se adapta la vista para textos editables y traducibles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import TeamPagePreview from './preview-templates/TeamPagePreview'
+import ActivitiesPagePreview from "./preview-templates/ActivitiesPagePreview"
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
@@ -12,3 +13,4 @@ CMS.registerMediaLibrary(cloudinary)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('team', TeamPagePreview)
+CMS.registerPreviewTemplate('activities',ActivitiesPagePreview)
